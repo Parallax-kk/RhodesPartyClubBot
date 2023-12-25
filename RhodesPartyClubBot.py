e732c05400d6521f223d594b6dd9fdf37a1a8759
Parallax_kk <kuronekoya.mail@gmail.com>
2023-12-26 00:20:21 +0900

    リファクタリング

--- RhodesPartyClubBot.py
+++ RhodesPartyClubBot.py
@@ -2,9 +2,57 @@ import discord
 import re
 from asyncio import sleep
 
-eventCategoryName = "イベント🎪🚩"
-yyyyMMddPattern = r"(\d{4})年(\d{1,2})月(\d{1,2})日"
-MMddPattern = r"(\d{1,2})月(\d{1,2})日(.*)"
+EVENT_CATEGORY_NAME = "イベント🎪🚩"
+YYYY_MM_DD_PATTERN  = r"(\d{4})年(\d{1,2})月(\d{1,2})日"
+MM_DD_PATTERN = r"(\d{1,2})月(\d{1,2})日(.*)"
+
+# 【】を削除
+def remove_brackets(text):
+    return text.replace("【", "").replace("】", "")
+
+# 正規パターンマッチング
+def match_date_pattern(pattern, text):
+    match = re.match(pattern, text)
+    return match
+
+# イベント通知を送信
+async def send_event_notification(creator, match):
+    if match:
+        date = match.group(1) + "月" + match.group(2) + "日"
+        event_title = match.group(3)
+        message = f"ハロ～、ドクター。\n{date}に{event_title}を開催するのね。\n"
+        message += "このテンプレートに沿ってイベント概要を書いてくれるかしら。\n"
+        message += "フフ、あなたの計画が上手く行くことを願っているわ。"
+    else:
+        message = "ハロ～、ドクター。\nイベントの日程がまだ決まっていないようね。\n"
+        message += "後でいいから追記しておいてくれるかしら。\n"
+        message += "一応テンプレートも置いておくから、空欄を埋めておいてね。"
+    await creator.send(message)
+
+# テンプレートを送信
+async def send_event_template(creator):
+    await creator.send("--------------------------\n" +
+                       "趣旨：\n" + 
+                       "期限：\n" +
+                       "日程：yyyy年MM月dd日\n" +
+                       "場所：\n" +
+                       "予算：\n" +
+                       "人数：\n" +
+                       "特記：\n" + 
+                       "--------------------------")
+    
+# メンバーにイベント通知を送信する
+async def notify_members(channel, url, event_title, month, day):
+    if channel.guild:
+        all_members = channel.guild.members
+        for member in all_members:
+            if not member.name == "ロドス宴会部長":
+                try:
+                    await member.send(f"ハロ～、ドクター。\n{month}月{day}日に{event_title}が開催されるらしいわ。\n"
+                                      "忘れないうちにカレンダーにさっさと登録しちゃいましょう。")
+                    await member.send(url)
+                except discord.Forbidden:
+                    print(f"Could not send DM to {member.display_name}")
 
 # インテントの生成
 intents = discord.Intents.default()
@@ -24,70 +72,32 @@ async def on_ready():
 async def on_guild_channel_create(channel):
     await sleep(1)
 
-    # チャンネルカテゴリ取得
     category = channel.category
-    if category.name != eventCategoryName:
+    if category.name != EVENT_CATEGORY_NAME:
         return
 
-    # チャンネルタイトル取得
-    channelTitle = channel.name.replace("【", "").replace("】", "")
+    channel_title = remove_brackets(channel.name)
+    mm_dd_match = match_date_pattern(MM_DD_PATTERN, channel_title)
 
-    # マッチングを試みる
-    match = re.match(MMddPattern, channelTitle)
-
-    # DMを送信
-    # チャンネルを作成したユーザーのID
     async for entry in channel.guild.audit_logs(action=discord.AuditLogAction.channel_create):
-        # entry.userがチャンネルを作成したユーザー
         creator = entry.user
         if entry.target.id == channel.id:
-            if match:
-                # マッチした場合は日付とテキストを返す
-                date = match.group(1) + "月" + match.group(2) + "日"
-                eventTitle = match.group(3)
-                await creator.send("ハロ～、ドクター。\n" + date +"に"+eventTitle+"を開催するのね。\n"+
-                                   "このテンプレートに沿ってイベント概要を書いてくれるかしら。\n" +
-                                   "フフ、あなたの計画が上手く行くことを願っているわ。")
-                await creator.send("--------------------------\n" +
-                                   "趣旨：\n" + 
-                                   "期限：\n" +
-                                   "日程：yyyy年MM月dd日\n" +
-                                   "場所：\n" +
-                                   "予算：\n" +
-                                   "人数：\n" +
-                                   "特記：\n" + 
-                                   "--------------------------")
-            else:
-                # マッチしなかった場合はNoneを返す       
-                await creator.send("ハロ～、ドクター。\nイベントの日程がまだ決まっていないようね。\n" + 
-                                   "後でいいから追記しておいてくれるかしら。\n" + 
-                                   "一応テンプレートも置いておくから、空欄を埋めておいてね。")
-                await creator.send("--------------------------\n" +
-                                   "趣旨：\n" + 
-                                   "期限：\n" +
-                                   "日程：yyyy年MM月dd日\n" +
-                                   "場所：\n" +
-                                   "予算：\n" +
-                                   "人数：\n" +
-                                   "特記：\n" + 
-                                   "--------------------------")
+            await send_event_notification(creator, mm_dd_match)
+            await send_event_template(creator)
 
-# メッセージを受信した時に呼ばれる
+# メッセージを受信したときに呼ばれる
 @client.event
 async def on_message(message):
-    # 自分のメッセージを無効
     if message.author == client.user:
         return
 
-    if isinstance(message.channel, discord.TextChannel) and message.channel.category and message.channel.category.name == eventCategoryName:
-        # テンプレートのパターンに一致するか確認
-
-        if "趣旨：" in message.content and "期限：" in message.content and "日程：" in message.content and "場所：" in message.content and "予算：" in message.content and "人数：" in message.content and "特記：" in message.content:
-            # メッセージから各項目を抽出
+    if isinstance(message.channel, discord.TextChannel) and message.channel.category and message.channel.category.name == EVENT_CATEGORY_NAME:
+        if "趣旨：" in message.content and "期限：" in message.content and "日程：" in message.content \
+                and "場所：" in message.content and "予算：" in message.content and "人数：" in message.content and "特記：" in message.content:
             lines = message.content.split("\n")
             purpose, deadline, schedule, location, budget, attendees, notes = "", "", "", "", "", "", ""
-            
-            addGoogleCalendarFlag = True
+
+            add_google_calendar_flag = True
 
             for line in lines:
                 if line.startswith("趣旨："):
@@ -96,9 +106,9 @@ async def on_message(message):
                     deadline = line[len("期限："):].strip()
                 elif line.startswith("日程："):
                     schedule = line[len("日程："):].strip()
-                    match = re.match(yyyyMMddPattern, schedule)
+                    match = match_date_pattern(YYYY_MM_DD_PATTERN, schedule)
                     if not match:
-                        addGoogleCalendarFlag = False
+                        add_google_calendar_flag = False
                 elif line.startswith("場所："):
                     location = line[len("場所："):].strip()
                 elif line.startswith("予算："):
@@ -108,28 +118,21 @@ async def on_message(message):
                 elif line.startswith("特記："):
                     notes = line[len("特記："):].strip()
 
-            if addGoogleCalendarFlag:
-                MMddMatch = re.match(MMddPattern, message.channel.name.replace("【", "").replace("】", ""))
-                yyyyMMddMatch = re.match(yyyyMMddPattern, schedule)
-                eventTitle = MMddMatch.group(3)
-                year = yyyyMMddMatch.group(1)
-                month = "{:02}".format(int(yyyyMMddMatch.group(2)))
-                day = "{:02}".format(int(yyyyMMddMatch.group(3)))
-                url = "https://www.google.com/calendar/event?action=TEMPLATE" + "&text=" + eventTitle + "&dates=" + year + month + day +  "/" + year + month + day + "&details=" + message.content.replace("\n", "%0D%0A") + "&location=" + location
-                if message.guild:
-                    all_members = message.guild.members
-                    for member in all_members:
-                        if not member.name == "ロドス宴会部長":
-                            try:
-                                await member.send("ハロ～、ドクター。\n" + month + "月" + day + "日" +"に" + eventTitle + "が開催されるらしいわ。\n"+
-                                                  "忘れないうちにカレンダーにさっさと登録しちゃいましょう。")
-                                await member.send(url)
-                            except discord.Forbidden:
-                                # 送信が失敗した場合、Forbidden例外が発生します（通常、DMを許可していない場合）
-                                print(f"Could not send DM to {member.display_name}")
+            if add_google_calendar_flag:
+                mm_dd_match = match_date_pattern(MM_DD_PATTERN, remove_brackets(message.channel.name))
+                yyyy_mm_dd_match = match_date_pattern(YYYY_MM_DD_PATTERN, schedule)
+                event_title = mm_dd_match.group(3)
+                year = yyyy_mm_dd_match.group(1)
+                month = "{:02}".format(int(yyyy_mm_dd_match.group(2)))
+                day = "{:02}".format(int(yyyy_mm_dd_match.group(3)))
+                url = f"https://www.google.com/calendar/event?action=TEMPLATE" \
+                      f"&text={event_title}&dates={year}{month}{day}/{year}{month}{day}" \
+                      f"&details={message.content.replace('\n', '%0D%0A')}&location={location}"
+
+                await notify_members(message.channel, url, event_title, month, day)
 
 # クライアントの実行
-# 同一ディレクトリにトークンが記載された"Token.txt"を配置
+# 同一ディレクトリにトークンが記載された"token.txt"を配置
 file_path = "token.txt"
 try:
     with open(file_path, "r") as file:
